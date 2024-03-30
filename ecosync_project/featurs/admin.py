1c17ff6dec9293422ba13943f747ae03a6aeef3d
SHADIK <shadik.sk420@gmail.com>
2024-03-30 19:21:12 +0600

    calculated billing added

--- ecosync_project/featurs/admin.py
+++ ecosync_project/featurs/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+from ecosync.models import *
 
 @admin.register(Vehicle)
 class VehicleAdmin(admin.ModelAdmin):
@@ -8,7 +9,16 @@ class VehicleAdmin(admin.ModelAdmin):
 
 @admin.register(SecondaryTransferStation)
 class SecondaryTransferStationAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['Manager']
     list_display = ['STSID', 'WardNumber', 'Capacity', 'GPSLatitude', 'GPSLongitude', 'Manager', 'CreatedAt', 'UpdatedAt']
+
+    # def formfield_for_foreignkey(self, db_field, request, **kwargs):
+    #     if db_field.name == 'Manager':
+    #         # Filter queryset to show only managers who have the role of "STS manager"
+    #         queryset = CustomUser.objects.filter(role__Name='STS manager')
+    #         # print(queryset)  # Print the queryset for debugging purposes
+    #         kwargs['queryset'] = queryset
+    #     return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 @admin.register(Landfill)
 class LandfillAdmin(admin.ModelAdmin):
@@ -25,18 +35,30 @@ class OilAllocationAdmin(admin.ModelAdmin):
 
 @admin.register(Billing)
 class BillingAdmin(admin.ModelAdmin):
-    autocomplete_fields = ['OilAllocation', 'Vehicle']
-    list_display = ['BillID', 'Vehicle', 'calculated_cost', 'WeekNumber', 'VolumeOfWaste', 'OilAllocation', 'OilConsumed', 'TotalCost', 'Distance', 'CreatedAt', 'UpdatedAt']
+    autocomplete_fields = ['Vehicle']
+    list_display = ['BillID', 'Vehicle', 'calculated_cost', 'WeekNumber', 'VolumeOfWaste', 'Distance', 'CreatedAt', 'UpdatedAt']
     readonly_fields = ['calculated_cost']
     
+    class Meta:
+        ordering = ['BillID']
+    
+    def cost_per_kilometer(self, obj):
+        # Assuming C_unloaded and C_loaded are defined somewhere
+        # Calculate the fraction of load relative to the truck's capacity
+        load_fraction = obj.VolumeOfWaste / obj.Vehicle.Capacity
 
+        C_loaded = obj.Vehicle.FuelCostLoaded
+        C_unloaded = obj.Vehicle.FuelCostUnloaded
+        # Interpolate the fuel cost per kilometer based on load
+        cost_per_kilometer = C_unloaded + load_fraction * (C_loaded - C_unloaded)
+
+        return cost_per_kilometer
+    
     def calculated_cost(self, obj):
-        # Perform your calculation here
-        if obj.VolumeOfWaste is not None and obj.OilConsumed is not None:
-            return obj.VolumeOfWaste * obj.OilConsumed
-        else:
-            return "VolumeOfWaste * OilConsumed"  # Or any default value you prefer
-    calculated_cost.short_description = 'calculated cost'
+        total_cost = self.cost_per_kilometer(obj) * obj.Distance
+        return round(total_cost, 3)
+    
+    calculated_cost.short_description = "Oil Allocation (TK)"
     
 @admin.register(DumpingEntryRecord)
 class DumpingEntryRecordAdmin(admin.ModelAdmin):
