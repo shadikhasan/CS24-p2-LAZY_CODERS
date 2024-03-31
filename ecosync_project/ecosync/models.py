ec9375b8c0c24169bee03286507a042d55a20d31
SHADIK <shadik.sk420@gmail.com>
2024-03-31 07:48:59 +0600

    docker and postgress sql added with existing database flush

--- ecosync_project/ecosync/models.py
+++ ecosync_project/ecosync/models.py
@@ -7,7 +7,7 @@ class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=255, blank=True)
     last_name = models.CharField(max_length=255, blank=True)
     email = models.EmailField(unique=True)
-    role = models.ForeignKey(Role, on_delete=models.SET_DEFAULT, default=4)  # Default role ID set to id 4 (id 4 = Unassigned)
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null = True, blank = True)  # Default role ID set to id 4 (id 4 = Unassigned)
 
     def __str__(self):
         return self.username
--- ecosync_project/rbac/serializers.py
+++ ecosync_project/rbac/serializers.py
@@ -1,6 +1,6 @@
 # serializers.py
 from rest_framework import serializers
-from .models import Role, Permission
+from featurs.models import Role, Permission
 
 class PermissionSerializer(serializers.ModelSerializer):
     class Meta:
